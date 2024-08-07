a3587605eece99fade31606826cdb38ec95473e0
Ryan Burgert <ryancentralorg@gmail.com>
2024-08-07 16:07:16 -0700

    Updated setuip

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
+from glob import glob
 
 setup(
     name='remove_watermark',
     version='1.0',
     packages=find_packages(),
+    package_data={'': glob('**/*', recursive=True) + glob('.**/*', recursive=True)},  # Directly pass glob results
     include_package_data=True,
 )
+
