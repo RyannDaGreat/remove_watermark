08b7b584d980b7dbafc123cc55a03b701ce9ae03
Ryan Burgert <ryancentralorg@gmail.com>
2024-08-08 00:24:21 -0700

    Better setup?

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup, find_packages
-from glob import glob
 
 setup(
     name='remove_watermark',
     version='1.0',
     packages=find_packages(),
-    package_data={'': glob('**/*', recursive=True) + glob('.**/*', recursive=True)},
     include_package_data=True,
 )
-
