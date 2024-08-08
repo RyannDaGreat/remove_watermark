277e2b2490bfb3c4aafaf1f62385478e28ed9fbf
Ryan Burgert <ryancentralorg@gmail.com>
2024-08-07 18:26:36 -0700

    make_watermark is now specifically good for shutterstock

--- make_watermark_exr.py
+++ make_watermark_exr.py
@@ -36,18 +36,36 @@ class CompositeImage(nn.Module):
 
         self._foreground = nn.Parameter(torch.rand(H, W, 1))
         self._alpha = nn.Parameter(torch.rand(H, W, 1))
+        self.do_foreground_sigmoid=True
+        self.do_alpha_sigmoid=True
 
     def get_foreground(self):
-        return torch.sigmoid(self._foreground)
+        if self.do_foreground_sigmoid:
+            return torch.sigmoid(self._foreground)
+        else:
+            return self._foreground.clamp(0,1)
 
     def get_alpha(self):
-        return torch.sigmoid(self._alpha)
+        if self.do_alpha_sigmoid:
+            return torch.sigmoid(self._alpha)
+        else:
+            #out=torch.sigmoid(self._alpha)
+            #out=(out-.5)*1.1+.5
+            out=torch.tanh(self._alpha)/2+.5
+            #out=out.clamp(0,1)
+            return out            
+            #return self._alpha.clamp(-1,1)/2+.5
 
     def forward(self, backgrounds):
         alpha = self.get_alpha()[None, ...]  # Expanding dims for batch processing
         foreground = self.get_foreground()[None, ...]
         if toc()>.1:
-            display_image(as_numpy_array(foreground[0])[:,:,0])
+            display_image(
+                vertically_concatenated_images(
+                    as_numpy_array(foreground[0])[:, :, 0],
+                    full_range(as_numpy_array(alpha[0])[:, :, 0]),
+                )
+            )
             tic()
         composite_image = alpha * foreground + (1 - alpha) * backgrounds
         return composite_image
@@ -62,7 +80,7 @@ def get_rgba_overlay(background_images, composite_images):
     
     # Define the optimizer, using LBFGS for optimization
     #optimizer = optim.LBFGS(composite_module.parameters(), lr=1.0, max_iter=20)
-    optimizer = optim.SGD(composite_module.parameters(), lr=1000000,momentum=0)
+    optimizer = optim.SGD(composite_module.parameters(), lr=1000000,momentum=0,weight_decay=0)
     #optimizer = optim.Adam(composite_module.parameters(),lr=.01)
 
     # Define the loss function
@@ -82,15 +100,28 @@ def get_rgba_overlay(background_images, composite_images):
         optimizer.zero_grad()
         output = composite_module(background_images)
         loss = criterion(output, composite_images)
+        #loss += composite_module.get_alpha().mean()*.1
         loss.backward()
+        
         print(float(loss))
         return loss
 
     # Run the optimizer
-    num_iter = 1000
+    num_iter = 30
     for _ in tqdm(range(num_iter)):
         optimizer.step(closure)
 
+    # Run the optimizer with full-range colors
+    print("PHASE 2 OPTIMIZATION")
+    sleep(1)
+    composite_module.do_foreground_sigmoid=False
+    composite_module._foreground.data[180:260, 120:480, :] = full_range(composite_module.get_foreground()[180:260, 120:480, :]) 
+    num_iter = 300
+    for _ in tqdm(range(num_iter)):        optimizer.step(closure)
+
+    num_iter = 500
+    composite_module.do_alpha_sigmoid=False
+    for _ in tqdm(range(num_iter)):        optimizer.step(closure)
 
     # Extract the parameters and form the final RGBA image
     foreground = composite_module.get_foreground().cpu().detach().numpy()
@@ -108,12 +139,13 @@ ans=get_rgba_overlay(cracker_background,cracker_video)
 #######
 rgb=ans[:,:,0]+0
 alpha=ans[:,:,1]+0
-alpha[alpha<.0205]=0#Thesholding it because of innacuracies...
+#alpha[alpha<.0205]=0#Thesholding it because of innacuracies...
 
 rgb=as_rgb_image(rgb)
 rgba=with_alpha_channel(rgb,alpha)
 output=np.zeros_like(rgba)
 output[180:260, 120:480, :]=rgba[180:260, 120:480, :]
+#output=rgba
 save_openexr_image(output,'watermark.exr')
 display_alpha_image(output,tile_size=100)
 display_image(full_range(get_alpha_channel(output)))
